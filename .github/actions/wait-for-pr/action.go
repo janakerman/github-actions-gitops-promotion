37d5bb33453e287fdccc0e6e78f556949764d3fb
Jan Akerman <j.f.akerman@gmail.com>
2021-05-26 18:46:06 +0100

    debug

--- .github/actions/wait-for-pr/action.go
+++ .github/actions/wait-for-pr/action.go
@@ -11,18 +11,18 @@ import (
 )
 
 func main() {
-	prNumString := os.Getenv("INPUT_PRNUMBER")
+	prNumString := os.Getenv("PR_NUM")
 	prNum, err := strconv.Atoi(prNumString)
 	if err != nil {
-		log.Fatalf("INPUT_PRNUMBER %s is not a number", prNumString)
+		log.Fatalf("PR_NUM %s is not a number", prNumString)
 	}
-	owner := os.Getenv("INPUT_OWNER")
+	owner := os.Getenv("OWNER")
 	if owner == "" {
-		log.Fatalf("INPUT_OWNER %s is a required value", owner)
+		log.Fatalf("OWNER %s is a required value", owner)
 	}
-	repo := os.Getenv("INPUT_REPO")
+	repo := os.Getenv("REPO")
 	if repo == "" {
-		log.Fatalf("INPUT_REPO %s is a required value", repo)
+		log.Fatalf("REPO %s is a required value", repo)
 	}
 
 	client := github.NewClient(nil)
